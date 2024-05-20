3bcff4fd936efdbd8f8fab0ed1a05bba40524cbd
Zathiald <A01705403@tec.mx>
2024-05-20 12:31:45 -0600

    Empezar a procesar datos

--- crystal_crosswind.py
+++ crystal_crosswind.py
@@ -9,7 +9,24 @@ def read_input():
         winds.append((w_x, w_y, b, boundaries))
     return d_x, d_y, k, winds
 
+def generate_structure(d_x, d_y, winds):
+    structure_min = [['.' for _ in range(d_x)] for _ in range(d_y)]
+    structure_max = [['.' for _ in range(d_x)] for _ in range(d_y)]
+    
+    for w_x, w_y, _, boundaries in winds:
+        for x, y in boundaries:
+            structure_min[y-1][x-1] = '#'
+            structure_max[y-1][x-1] = '#'
+    
+    return structure_min, structure_max
+
+def print_structure(structure):
+    for row in structure:
+        print(''.join(row))
+
 d_x, d_y, k, winds = read_input()
+structure_min, structure_max = generate_structure(d_x, d_y, winds)
+
 print("d_x:", d_x)
 print("d_y:", d_y)
 print("k:", k)
@@ -18,6 +35,13 @@ for w_x, w_y, b, boundaries in winds:
     print("Direction:", w_x, w_y)
     print("Number of boundaries:", b)
     print("Boundaries:", boundaries)
+
+
+print("Minimum structure:")
+print_structure(structure_min)
+
+print("\nMaximum structure:")
+print_structure(structure_max)
 
 
 
